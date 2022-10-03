cc2a510f43e86da48c3a9c98a727dca9a24ac0a6
EnzeXu <xue20@wfu.edu>
2022-10-03 10:00:04 -0400

    updates

--- const.py
+++ const.py
@@ -1,4 +1,4 @@
 CONCERNED_STATION_LIST = ["southern kitchen", "allgood"]
-CONCERNED_DISH_KEYWORD_LIST = ["steak"]
+CONCERNED_DISH_KEYWORD_LIST = ["steak", "beef"]
 
 MAIL_RECEIVER_LIST = ["xue20@wfu.edu"]
